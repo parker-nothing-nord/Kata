8569d66657dc71a8decefd0a8f3117043861c031
pbrown <pbrown@acromedia.com>
2026-02-02 04:40:46 -0800

    helper function and CL args

--- CSharp/CIsForCodewars/Program.cs
+++ CSharp/CIsForCodewars/Program.cs
@@ -2,26 +2,53 @@
 
 public class Kata
 {
+    int size;
     public static string GenerateC(int size)
     {
         // TODO: Implement the solution
-        return "Test output";
+    string output = "";
+    output += lineOne(size);
+    
+    
+
+        return output;
+    }
+
+
+    static string lineOne(int size) {
+        string line = "";
+        for (int i = 0; i < 5 * size; i++) {
+            line += "C";
+        }
+        return line;
     }
 }
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         Console.WriteLine("Running simple test...\n");
 
         string result = Kata.GenerateC(1);
-        Console.WriteLine($"GenerateC(1) returned: '{result}'");
+        if (args.Length > 0)
+        {
+            int size = int.Parse(args[0]);
+            result = Kata.GenerateC(size);
+        }
+        else
+        {
+            Console.WriteLine("No argument provided, using default size of 1");
+            result = Kata.GenerateC(1);
+        }
+        
+        Console.WriteLine($"GenerateC(1) returned: \n'{result}'");
         Console.WriteLine("\nExpected:");
         Console.WriteLine("CCCCC");
         Console.WriteLine("C");
         Console.WriteLine("C");
         Console.WriteLine("C");
         Console.WriteLine("CCCCC");
+
     }
 }
