706f03c1011bb2d50d32f15521126c45cae4ffb7
pbrown <pbrown@acromedia.com>
2026-02-02 14:07:19 -0800

    Add ternary improvement

--- CSharp/WhoLikesIt/Program.cs
+++ CSharp/WhoLikesIt/Program.cs
@@ -3,58 +3,48 @@ using System.Linq;
 
 public class Kata
 {
-    public static string Likes(string[] names)
+    public static string Likes(string[] name)
     {
      
-        // return SolutionOne(names);
-        return SolutionTwo(names);
+        // return SolutionOne(name);
+        return SolutionTwo(name);
         throw new NotImplementedException();
 
 
         /**
         * Set out patterns, then set array against pattern. Scalable.
         */
-        static string SolutionTwo(string[] names)
+        static string SolutionTwo(string[] name)
         {
-            string pattern = "";
-            // Short circuit if 'others' calculation needed.
-            if (names.Length >= 4)
-            {
-                int othersCount = names.Length - 2;
-                pattern = "{0}, {1} and {2} others like this";
-                names = [names[0], names[1], othersCount.ToString()];
-            }
-            else
-            {
             string[] patterns = [
                 "no one likes this",
                 "{0} likes this",
                 "{0} and {1} like this",
                 "{0}, {1} and {2} like this",
+                "{0}, {1} and {2} others like this",
             ];
 
-            pattern = patterns[Math.Min(names.Length, 4)];
-            }
-            return string.Format(pattern, names);
+            string selectedPattern = patterns[Math.Min(name.Length, 4)];
+            return name.Length > 3 ? string.Format(selectedPattern, name[0], name[1], name.Length - 2) : string.Format(selectedPattern, name);
         }
 
         /**
          * Flat switch and replace.Intuitive.
          */
-        static string SolutionOne(string[] names)
+        static string SolutionOne(string[] name)
         {   
-            switch (names.Length)
+            switch (name.Length)
             {
                 case 0:
                     return "no one likes this";
                 case 1:
-                    return $"{names[0]} likes this";
+                    return $"{name[0]} likes this";
                 case 2:
-                    return $"{names[0]} and {names[1]} like this";
+                    return $"{name[0]} and {name[1]} like this";
                 case 3:
-                    return $"{names[0]}, {names[1]} and {names[2]} like this";
+                    return $"{name[0]}, {name[1]} and {name[2]} like this";
                 default:
-                    return $"{names[0]}, {names[1]} and {names.Length - 2} others like this";
+                    return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
             }
         }
     }
